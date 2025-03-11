67fb0341b02f954e90eea2c213a0842731125fbf
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-03-10 22:29:43 -0300

    chore: commit inicial - estrutura do projeto e configurações (Spring Boot, Flyway, PostgreSQL e Kafk

---
+++ creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/CreditosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.raaydesenvolvimento.creditos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditosApiApplication.class, args);
+	}
+
+}
---
+++ creditos-api/src/test/java/br/com/raaydesenvolvimento/creditos_api/CreditosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raaydesenvolvimento.creditos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
