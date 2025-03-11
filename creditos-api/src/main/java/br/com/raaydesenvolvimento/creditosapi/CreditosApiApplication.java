0210253984fd81a8271532caadc3a219ca5f0b51
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-03-10 23:12:37 -0300

    chore: ajuste nome pacote

--- creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/CreditosApiApplication.java
+++ creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/CreditosApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.raaydesenvolvimento.creditos_api;
+package br.com.raaydesenvolvimento.creditosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
