eed4c96620730fa61523c9720e67ec867ac46a91
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-03-11 03:01:49 -0300

    feat: adicionando propriedades swagger.

--- creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/controller/CreditoController.java
+++ creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/controller/CreditoController.java
@@ -2,6 +2,8 @@ package br.com.raaydesenvolvimento.creditosapi.controller;
 
 import br.com.raaydesenvolvimento.creditosapi.model.Credito;
 import br.com.raaydesenvolvimento.creditosapi.service.CreditoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -27,15 +29,21 @@ public class CreditoController {
     @Value("${kafka.topic.consulta}")
     private String topic;
 
+    @Operation(summary = "Retorna uma lista de créditos constituídos com base no número da NFS-e")
     @GetMapping("/{numeroNfse}")
-    public ResponseEntity<List<Credito>> getCreditosByNfse(@PathVariable String numeroNfse) {
+    public ResponseEntity<List<Credito>> getCreditosByNfse(
+            @Parameter(description = "Número identificador da NFS-e", required = true)
+            @PathVariable String numeroNfse) {
         List<Credito> creditos = creditoService.getCreditosByNfse(numeroNfse);
         kafkaTemplate.send(topic, "Consulta realizada para NFS-e: " + numeroNfse);
         return ResponseEntity.ok(creditos);
     }
 
+    @Operation(summary = "Retorna os detalhes de um crédito constituído específico com base no número do crédito constituído")
     @GetMapping("/credito/{numeroCredito}")
-    public ResponseEntity<Credito> getCreditoByNumero(@PathVariable String numeroCredito) {
+    public ResponseEntity<Credito> getCreditoByNumero(
+            @Parameter(description = "Número identificador do crédito constituído", required = true)
+            @PathVariable String numeroCredito) {
         Optional<Credito> credito = creditoService.getCreditoByNumero(numeroCredito);
         kafkaTemplate.send(topic, "Consulta realizada para Crédito: " + numeroCredito);
         return credito.map(ResponseEntity::ok)
