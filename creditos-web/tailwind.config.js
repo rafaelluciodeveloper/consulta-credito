2dd0b566294d8c4677700bae99af0d1226df6c34
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-03-11 03:51:15 -0300

    chore: criando e configurando creditos front end

---
+++ creditos-web/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
