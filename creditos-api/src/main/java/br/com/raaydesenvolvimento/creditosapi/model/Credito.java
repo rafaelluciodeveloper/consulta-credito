ad0666581a53d41afda78344a901a8d50f01fa34
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-03-10 23:14:19 -0300

    feat: criar entidade Credito

---
+++ creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/model/Credito.java
@@ -0,0 +1,31 @@
+package br.com.raaydesenvolvimento.creditosapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Credito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numeroCredito;
+    private String numeroNfse;
+    private LocalDate dataConstituicao;
+    private BigDecimal valorIssqn;
+    private String tipoCredito;
+    private boolean simplesNacional;
+    private BigDecimal aliquota;
+    private BigDecimal valorFaturado;
+    private BigDecimal valorDeducao;
+    private BigDecimal baseCalculo;
+
+}
