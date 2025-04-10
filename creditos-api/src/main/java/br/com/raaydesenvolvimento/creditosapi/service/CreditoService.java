b02620e67d1894611190c150f785a2554ebc30b8
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-04-10 09:14:07 -0300

    feat: Alterar método getCreditoByNumero para retornar uma lista de Credito

--- creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/service/CreditoService.java
+++ creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/service/CreditoService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CreditoService {
@@ -18,7 +17,7 @@ public class CreditoService {
         return creditoRepository.findByNumeroNfse(numeroNfse);
     }
 
-    public Optional<Credito> getCreditoByNumero(String numeroCredito) {
+    public List<Credito> getCreditoByNumero(String numeroCredito) {
         return creditoRepository.findByNumeroCredito(numeroCredito);
     }
 }
