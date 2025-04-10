3f935fe3a63ad531fe2274a6eee533a5e6e01e6c
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-04-10 09:13:56 -0300

    feat: Alterar método findByNumeroCredito para retornar uma lista de Credito

--- creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/repository/CreditoRepository.java
+++ creditos-api/src/main/java/br/com/raaydesenvolvimento/creditosapi/repository/CreditoRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CreditoRepository extends JpaRepository<Credito, Long> {
     List<Credito> findByNumeroNfse(String numeroNfse);
-    Optional<Credito> findByNumeroCredito(String numeroCredito);
+
+    List<Credito> findByNumeroCredito(String numeroCredito);
 }
